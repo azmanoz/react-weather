3829e8b4524efc776f14b636c665ece5e9210df4
azmanamutalib <azman@scs.my>
2020-10-07 14:48:49 +0800

    Updates

---
+++ precache-manifest.befa59b43201f339a2b7961a2e574f3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6874b394f39a4966e00eb004a87b30e",
+    "url": "/react-weather/index.html"
+  },
+  {
+    "revision": "fdc24fc4c30807f478a5",
+    "url": "/react-weather/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "11f909f82ba13e2963e6",
+    "url": "/react-weather/static/js/2.c8a4d0c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather/static/js/2.c8a4d0c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdc24fc4c30807f478a5",
+    "url": "/react-weather/static/js/main.6a6866ff.chunk.js"
+  },
+  {
+    "revision": "a295175cb5ea0677526b",
+    "url": "/react-weather/static/js/runtime-main.c5a8c4e8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-weather/static/media/logo.5d5d9eef.svg"
+  }
+]);
